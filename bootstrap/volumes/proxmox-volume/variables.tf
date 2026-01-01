2c812fac3527089ffba200cacc5c110fa47b185f
sebiklamar <53593629+sebiklamar@users.noreply.github.com>
2026-01-01 09:50:41 +0000

    style: automated terraform formatting

--- bootstrap/volumes/proxmox-volume/variables.tf
+++ bootstrap/volumes/proxmox-volume/variables.tf
@@ -7,10 +7,10 @@ variable "env" {
 variable "volume" {
   type = object({
     datastore = optional(string, "local-zfs")
-    format  = optional(string, "raw")
-    name    = string
-    node    = string
-    size    = string
-    vmid    = optional(number, 9999)
+    format    = optional(string, "raw")
+    name      = string
+    node      = string
+    size      = string
+    vmid      = optional(number, 9999)
   })
 }
